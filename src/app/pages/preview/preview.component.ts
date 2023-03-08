f2f9daeb2a76cad23de80e523a6f77ca3e83fd41
allemeshov <allemeshov@gmail.com>
2023-03-08 17:21:37 +0100

    feat(preview): loader on preview.

--- src/app/pages/preview/preview.component.ts
+++ src/app/pages/preview/preview.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {PostcardService} from "../../services/postcard.service";
 import {ActivatedRoute} from "@angular/router";
 import {Postcard} from "../../interfaces/postcard";
@@ -9,28 +9,63 @@ import {environment} from "../../../environments/environment";
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
-export class PreviewComponent implements OnInit {
-  public postcard: Postcard = {
+export class PreviewComponent implements OnInit, AfterViewInit {
+  @ViewChildren('image') imagesOnPage!: QueryList<HTMLImageElement>;
+
+  postcard: Postcard = {
     wishFrom: '',
     wishTo: '',
     wishText: '',
     images: []
   };
 
+  private loadedImageCount = 0;
+  areImagesLoaded = false;
+
   constructor(
     private _postcardService: PostcardService,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
   ) {
+  }
+
+  imageLoaded(): void {
+    this.loadedImageCount++;
+    console.log(this.loadedImageCount)
+
+    this.areImagesLoaded = this.loadedImageCount === this.imagesOnPage.length;
   }
 
   ngOnInit(): void {
     this._route.params.subscribe(x => {
       let id = x['id']
-      this._postcardService.get(id).subscribe({ next: (c) => this.postcard = c})
+      this._postcardService.get(id).subscribe({
+        next: (c) => {
+          this.postcard = {...c}
+        }
+      });
+    });
+  }
+
+  ngAfterViewInit() {
+    this.imagesOnPage.changes.subscribe(value => {
+      if (value.length > 0) {
+        this.initLoader();
+      }
     })
   }
 
   getPathForImage(imageName: string) {
     return `${environment.apiUrl}/files/get/${imageName}`;
+  }
+
+  private initLoader() {
+    this.imagesOnPage.toArray().forEach(image => {
+      image = (image as any as ElementRef<HTMLImageElement>).nativeElement;
+      if (image.complete) {
+        this.imageLoaded();
+      } else {
+        image.addEventListener('load', this.imageLoaded.bind(this));
+      }
+    });
   }
 }
